0e4f9f4b97d5891ac00664a83c80495f303471c7
cybergInfinity <129176146+cybergInfinity@users.noreply.github.com>
2023-08-08 20:01:11 +0900

    Update AppRemote.java2

--- src/main/java/com/ldw/git/AppRemote.java
+++ src/main/java/com/ldw/git/AppRemote.java
@@ -3,5 +3,6 @@ package com.ldw.git;
 public class AppRemote {
 public static void main(String[] args) {
 	System.out.println("edit by Remote Repository");
+	System.out.println("edit by Remote Repository2");
 }
 }
