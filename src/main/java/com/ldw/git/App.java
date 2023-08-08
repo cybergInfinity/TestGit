00025905b923c5d54170fc82ff6811f13592f3e6
cybergInfinity <129176146+cybergInfinity@users.noreply.github.com>
2023-08-08 19:57:09 +0900

    Update App.java by Remote

--- src/main/java/com/ldw/git/App.java
+++ src/main/java/com/ldw/git/App.java
@@ -3,5 +3,6 @@ package com.ldw.git;
 public class App {
 public static void main(String[] args) {
 	System.out.println("edit by GitTest develop");
+	System.out.println("edit by Remote Repository");
 }
 }
