4bf2af3204f8623a4c5e0b3fe483dfd6199add8b
ldw <liuyi16001074@gmail.com>
2023-08-08 20:13:32 +0900

    edit by Gitlocal

--- src/main/java/com/ldw/git/App.java
+++ src/main/java/com/ldw/git/App.java
@@ -4,7 +4,7 @@ public class App {
 public static void main(String[] args) {
 	System.out.println("edit by GitTest develop");
 	System.out.println("edit by Remote Repository");
-	System.out.println("edit by GitTest ");
+	System.out.println("edit by Gitlocal ");
 	
 }
 }
