eb296d2b1792752ea8af13b4018ed2ef49738e75
cybergInfinity <129176146+cybergInfinity@users.noreply.github.com>
2023-08-08 20:10:58 +0900

    Create AppRemote1.java

---
+++ src/main/java/com/ldw/git/AppRemote1.java
@@ -0,0 +1,8 @@
+package com.ldw.git;
+
+public class AppRemote1 {
+public static void main(String[] args) {
+	System.out.println("edit by Remote Repository");
+	System.out.println("edit by Remote Repository2");
+}
+}
