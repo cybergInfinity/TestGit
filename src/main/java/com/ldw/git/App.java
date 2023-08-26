69d98366e52151f333a223b3e78b0afae312d3c3
ldw <liuyi16001074@gmail.com>
2023-08-26 15:04:10 +0900

    0826 delete by TestGit

--- src/main/java/com/ldw/git/App.java
+++ src/main/java/com/ldw/git/App.java
@@ -8,7 +8,6 @@ public static void main(String[] args) {
 	System.out.println("edit by Git ");
 	System.out.println("test conflict by GitTest");
 	System.out.println("test conflict by TestGit");
-	System.out.println("0826 by GitTest");
 }
 }
  
