25356d04bfd672ba4605e11efd4323a90a970448
ldw <liuyi16001074@gmail.com>
2023-08-26 15:08:42 +0900

    0826 update by GitTest

--- src/main/java/com/ldw/git/App.java
+++ src/main/java/com/ldw/git/App.java
@@ -8,6 +8,7 @@ public static void main(String[] args) {
 	System.out.println("edit by Git ");
 	System.out.println("test conflict by GitTest");
 	System.out.println("test conflict by TestGit");
+	System.out.println("0826 update by GitTest");
 }
 }
  
